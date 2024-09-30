6bb60b476238f7869df9d7a31329eb3843278ec4
mukeshgupta7 <39895994+mukeshgupta7@users.noreply.github.com>
2024-09-30 18:38:58 +0530

    Update __init__.py

--- GenAI/app/__init__.py
+++ GenAI/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/process_campaign": {"origins": "*"}})
     
     with app.app_context():
         from .routes import main
