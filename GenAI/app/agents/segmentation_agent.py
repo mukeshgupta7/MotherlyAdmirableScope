75d9822fa7f4fe6c5e8aeb5b630c4d0864b8b352
mukeshgupta7 <39895994+mukeshgupta7@users.noreply.github.com>
2024-10-01 11:46:06 +0530

    Update segmentation_agent.py

--- GenAI/app/agents/segmentation_agent.py
+++ GenAI/app/agents/segmentation_agent.py
@@ -1,3 +1,17 @@
+# from sklearn.cluster import KMeans
+# import pandas as pd
+
+# class SegmentationAgent:
+#     def __init__(self, campaign_data):
+#         self.campaign_data = campaign_data
+#         self.X = pd.DataFrame(campaign_data)[['Age', 'Income', 'Engagement_Rate']]
+#         self.kmeans = KMeans(n_clusters=3)
+#         self.kmeans.fit(self.X)
+
+#     def segment(self, customer_data):
+#         customer_features = [customer_data['Age'], customer_data['Income'], customer_data['Engagement_Rate']]
+#         return self.kmeans.predict([customer_features])[0]
+
 from sklearn.cluster import KMeans
 import pandas as pd
 
@@ -5,6 +19,8 @@ class SegmentationAgent:
     def __init__(self, campaign_data):
         self.campaign_data = campaign_data
         self.X = pd.DataFrame(campaign_data)[['Age', 'Income', 'Engagement_Rate']]
+        self.feature_names = self.X.columns.tolist()
+        self.X = self.X.values
         self.kmeans = KMeans(n_clusters=3)
         self.kmeans.fit(self.X)
 
